60c0188811a5b85f90b483853b0f7a6da222a804
Joshua <jojoshi1034@gmail.com>
2023-06-21 10:24:27 -0400

    Add hover coloring and ability to change side of grid

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-for (let i = 0; i < 16; i++) {
-    var row = document.createElement('div');
-    row.className = "row";
-    for (let j = 0; j < 16; j++) {
-        var cell = document.createElement('div');
-        cell.className = "cell";
-//        cell.textContent = "";
-        row.appendChild(cell);
+const container = document.querySelector('#container');
+
+function makeGrid() {
+    size = window.prompt("Enter number of squares on side", "");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
-    document.body.appendChild(row);
+    
+    for (let i = 0; i < size; i++) {
+        var row = document.createElement('div');
+        row.className = "row";
+        for (let j = 0; j < size; j++) {
+            var cell = document.createElement('div');
+            cell.setAttribute("onmouseenter", "event.target.style.backgroundColor = '#10FF00';");
+            cell.style.width = 640/size+"px";
+            cell.style.height = 640/size+"px";
+            cell.className = "cell";
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+}
+
+function touchHandler(e) {
+  if (e.type == "touchstart") {
+    alert("You touched the screen!");
+  } else if (e.type == "touchmove") {
+    alert("You moved your finger!");
+  } else if (e.type == "touchend" || e.type == "touchcancel") {
+    alert("You removed your finger from the screen!");
+  }
 }
